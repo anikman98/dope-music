ae3b8cf687873144e02f48067ee6f633e15ada9f
anikman <anikman98@gmail.com>
2022-09-15 22:26:53 +0530

    changed fueler logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className='footer'>
         <div className="footer-left"><b>lofi</b></div>
-        <div className="footer-center"><a href="https://fueler.io/" target={"_blank"} rel="noreferrer">Side project by <img src="https://fueler.io/images/fueler/main/fueler-icon.svg" alt="" /> <span className='fueler'>Fueler</span></a></div>
+        <div className="footer-center"><a href="https://fueler.io/" target={"_blank"} rel="noreferrer">Side project by <img src={require('../resource/fueler.png')} alt="" /> <span className='fueler'>Fueler</span></a></div>
         <div className="footer-right"><a href="https://github.com/anikman98/dope-music" target={"_blank"} rel="noreferrer">v1.0</a></div>
     </footer>
   )
